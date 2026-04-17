ab7c97f7019033e75907295ee734a67ae18cb1b7
Katrina Weers <katrinaweers@gmail.com>
2026-04-16 18:23:03 -0700

    add jacoco dependency

--- software/src/test/java/edu/sjsu/spring2026/group32/hardware/HardwareSignalSourceTest.java
+++ software/src/test/java/edu/sjsu/spring2026/group32/hardware/HardwareSignalSourceTest.java
@@ -55,7 +55,9 @@ class HardwareSignalSourceTest {
         
         // Simulates successful connection (connect() returns true) 
         when(mockConnectionManager.connect()).thenReturn(true);
-        when(mockConnectionManager.isConnected()).thenReturn(true);
+        when(mockConnectionManager.isConnected())
+            .thenReturn(false)
+            .thenReturn(true);
 
         // Returns a valid fake string of data for getNextLine() (e.g., "123456,1,0,2047").
         when(mockConnectionManager.getNextLine()).thenReturn("123456,1,0,2047");
